b7a00ec3fc702cd7c49dc55928f9bd5b506b98fa
Daniel Guardado <danguardado217@gmail.com>
2021-04-15 09:49:31 -0400

    finsihed processing orders

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-from process_csvs import pickup_csv, pickup_csv2
+from process_csvs import pickup_csv
 
-# , pickup_csv_prev
-
-# , invoice_csv, sales_order_csv
-from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
-from datetime import datetime
-
-
-# def listener(event):
-#     if not event.exception:
-#         job = scheduler.get_job(event.job_id)
-#         if job.name == "pickupCSV":
-#             now = datetime.now()
-#             print(now)
-#     if event.exception:
-#         print("woops something broke")
-#         scheduler.shutdown()
-
-
-# scheduler = BlockingScheduler()
-# scheduler.add_listener(listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
-# scheduler.add_job(pickupCSV, "interval", hours=24, start_date="2021-04-02 17:50:00")
-# scheduler.add_job(invoiceCSV, "interval", hours=24, start_date="2021-04-02 17:50:20")
-# scheduler.add_job(salesOrderCSV, "interval", hours=24, start_date="2021-04-02 17:50:40")
-# scheduler.start()
-
-pickup_csv2()
-# pickup_csv_prev()
-# # invoice_csv()
-# sales_order_csv()
+pickup_csv()
--- process_csvs.py
+++ process_csvs.py
@@ -2,9 +2,10 @@ import requests
 import os, uuid
 import pandas as pd
 from datetime import datetime, timedelta
+import io
 
 
-from get_csv import get_pickup, get_pickup_prev
+from get_csv import get_pickup
 
 
 local_path = "./data"
@@ -12,18 +13,46 @@ local_path = "./data"
 
 def pickup_csv():
 
-    csv = get_pickup(1)
-    local_file_name = f"PickupReport2.csv"
+    csv = get_pickup(1, "current")
+    csv2 = get_pickup(2, "current")
+    csv3 = get_pickup(1, "previous")
+    csv4 = get_pickup(2, "previous")
+
+    local_file_name = f"CurrentMonth.csv"
+    local_file_name2 = f"LastMonth.csv"
+
     upload_file_path = os.path.join(local_path, local_file_name)
+    upload_file_path2 = os.path.join(local_path, local_file_name2)
 
-    with open(upload_file_path, "wb") as file:
-        file.write(csv)
-
-    df = pd.read_csv(upload_file_path, skiprows=5, header=0, engine="python")
-    df.rename(columns={"Unnamed: 0": "test", "Unnamed: 15": "Qty"}, inplace=True)
+    df = pd.read_csv(
+        io.StringIO(csv.decode("utf-8")), skiprows=5, header=0, engine="python"
+    )
+    df.rename(columns={"Unnamed: 15": "Qty"}, inplace=True)
     df = df[["Part Number", "Qty"]]
     df.drop(df.tail(1).index, inplace=True)
-    print(df)
+
+    df2 = pd.read_csv(
+        io.StringIO(csv2.decode("utf-8")), skiprows=5, header=0, engine="python"
+    )
+    df2.rename(columns={"Unnamed: 21": "Qty"}, inplace=True)
+    df2 = df2[["Part Number", "Qty"]]
+    df2.drop(df2.tail(1).index, inplace=True)
+    df = df.append(df2)
+
+    df3 = pd.read_csv(
+        io.StringIO(csv3.decode("utf-8")), skiprows=5, header=0, engine="python"
+    )
+    df3.rename(columns={"Unnamed: 15": "Qty"}, inplace=True)
+    df3 = df3[["Part Number", "Qty"]]
+    df3.drop(df3.tail(1).index, inplace=True)
+
+    df4 = pd.read_csv(
+        io.StringIO(csv4.decode("utf-8")), skiprows=5, header=0, engine="python"
+    )
+    df4.rename(columns={"Unnamed: 21": "Qty"}, inplace=True)
+    df4 = df4[["Part Number", "Qty"]]
+    df4.drop(df4.tail(1).index, inplace=True)
+    df3 = df3.append(df4)
 
     df.to_csv(
         upload_file_path,
@@ -32,48 +61,9 @@ def pickup_csv():
         quotechar='"',
     )
 
-
-def pickup_csv2():
-
-    csv = get_pickup(2)
-    local_file_name = f"PickupReport2.csv"
-    upload_file_path = os.path.join(local_path, local_file_name)
-
-    with open(upload_file_path, "wb") as file:
-        file.write(csv)
-
-    df = pd.read_csv(upload_file_path, skiprows=5, header=0, engine="python")
-    df.rename(columns={"Unnamed: 0": "test", "Unnamed: 21": "Qty"}, inplace=True)
-    df = df[["Part Number", "Qty"]]
-    df.drop(df.tail(1).index, inplace=True)
-    print(df)
-
-    df.to_csv(
-        upload_file_path,
+    df3.to_csv(
+        upload_file_path2,
         index=False,
         header=True,
         quotechar='"',
     )
-
-
-# def pickup_csv_prev():
-
-#     csv = get_pickup_prev(1)
-#     local_file_name = f"PickupReportPrev.csv"
-#     upload_file_path = os.path.join(local_path, local_file_name)
-
-#     with open(upload_file_path, "wb") as file:
-#         file.write(csv)
-
-#     df = pd.read_csv(upload_file_path, skiprows=5, header=0, engine="python")
-#     df.rename(columns={"Unnamed: 0": "test", "Unnamed: 15": "Qty"}, inplace=True)
-#     df = df[["Part Number", "Qty"]]
-#     df.drop(df.tail(1).index, inplace=True)
-#     print(df)
-
-#     df.to_csv(
-#         upload_file_path,
-#         index=False,
-#         header=True,
-#         quotechar='"',
-#     )
